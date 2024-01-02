47a3d57ec93f8112fb9613cc26b8cf74410c451b
HideOne <844085696@qq.com>
2024-01-02 17:56:17 +0800

    覆盖

---
+++ docs/assets/设计模式.html-fIBMlS5A.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as o,b as e,d as r}from"./app-0fW7japb.js";const _={},c=e("h2",{id:"命令模式",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#命令模式","aria-hidden":"true"},"#"),r(" 命令模式")],-1),s=[c];function n(d,i){return a(),o("div",null,s)}const f=t(_,[["render",n],["__file","设计模式.html.vue"]]);export{f as default};
