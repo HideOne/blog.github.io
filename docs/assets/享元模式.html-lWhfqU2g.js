482e4e3d962e0dec28ce81da9743ae9d1d5f2f9b
HideOne <844085696@qq.com>
2024-01-23 18:12:58 +0800

    覆盖

---
+++ docs/assets/享元模式.html-lWhfqU2g.js
@@ -0,0 +1,94 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as e,a as s}from"./app-HeG0ypbT.js";const l="/assets/image-1-I2lJiA9y.png",d="/assets/image-2-ZinCj8W5.png",r="/assets/image-3-p_tfDSUB.png",a={},v=s('<h2 id="定义" tabindex="-1"><a class="header-anchor" href="#定义" aria-hidden="true">#</a> 定义</h2><p>使用共享高效地支持大量的细粒度对象</p><h2 id="使用场景" tabindex="-1"><a class="header-anchor" href="#使用场景" aria-hidden="true">#</a> 使用场景</h2><p>广袤的森林树木 如果所有树木都单独享有数据内存占用无比巨大 并且这些数据在一帧内传入gpu完全不可能</p><p>小方框内的数据为同一份数据 <img src="'+l+`" alt="Alt text" loading="lazy"></p><p>我们可以把树木通用数据放到一个单独的类中</p><div class="language-c++ line-numbers-mode" data-ext="c++"><pre class="language-c++"><code>    class TreeModel
+    { 
+    private:
+      Mesh mesh_;
+      Texture bark_;
+      Texture leaves_;
+    };
+
+
+    class Tree
+    {
+    private:
+      TreeModel* model_;
+
+      Vector position_;
+      double height_;
+      double thickness_;
+      Color barkTint_;
+      Color leafTint_;
+    };
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>整个游戏只需要一份这个数据所有的实例树都指向它</p><p>看起来非常像类型对象模式。两者都是把部分状态给另一个由大量实例所共享的对象 但是两个模式背后的意图不同: 类型对象是减少定义新类型的数量 而享元是更注重执行效率</p><p>图例展示 <img src="`+d+`" alt="Alt text" loading="lazy"></p><h3 id="地形例子" tabindex="-1"><a class="header-anchor" href="#地形例子" aria-hidden="true">#</a> 地形例子</h3><div class="language-c++ line-numbers-mode" data-ext="c++"><pre class="language-c++"><code>    class Terrain
+    {
+    public:
+      Terrain(int movementCost, bool isWater,
+             Texture texture)
+      : moveCost_(moveCost),
+        isWater_(isWater),
+        texture_(texture)
+      {}
+
+      int getMoveCost() const { return moveCost_; }
+      bool isWater() const { return isWater_; }
+      const Texture&amp; getTexture() const
+      {
+      return texture_;
+      }
+
+    private:
+      int moveCost_;
+      bool isWater_;
+      Texture texture_;
+    };
+
+    class World
+    {
+        public:
+        World()
+        : grassTerrain_(1, false, GRASS_TEXTURE),
+            hillTerrain_(3, false, HILL_TEXTURE),
+            riverTerrain_(2, true, RIVER_TEXTURE)
+        {}  
+        
+        // 地形生成
+        void World::generateTerrain()
+        {
+            // Fill the ground with grass.
+            for (int x = 0; x &lt; WIDTH; x++)
+            {
+                for (int y = 0; y &lt; HEIGHT; y++)
+                {
+                    // Sprinkle some hills.
+                    if (random(10) == 0)
+                    {
+                        tiles_[x][y] = &amp;hillTerrain_;
+                    }
+                    else
+                    {
+                    tiles_[x][y] = &amp;grassTerrain_;
+                    }
+                }
+            }
+
+        // Lay a river.
+        int x = random(WIDTH);
+        for (int y = 0; y &lt; HEIGHT; y++) {
+        tiles_[x][y] = &amp;riverTerrain_;
+        }
+    }
+
+    const Terrain&amp; World::getTile(int x, int y) const
+    {
+      return *tiles_[x][y];
+    }
+
+        private:
+        Terrain grassTerrain_;
+        Terrain hillTerrain_;
+        Terrain riverTerrain_;
+        // Other stuff...
+
+        
+    };
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><figure><img src="`+r+'" alt="Alt text" tabindex="0" loading="lazy"><figcaption>Alt text</figcaption></figure>',13),c=[v];function u(t,m){return n(),e("div",null,c)}const _=i(a,[["render",u],["__file","享元模式.html.vue"]]);export{_ as default};
---
+++ src/posts/游戏/Unity/canvas.md
@@ -0,0 +1,56 @@
+---
+title: canvas
+category:
+  - Unity
+date: 2024-01-23 14:16:00
+headerDepth: 2
+---
+
+## Canvas Scaler
+[官网](https://docs.unity3d.com/cn/current/Manual/script-CanvasScaler.html)
+
+
+## 属性
+![Alt text](image-2.png)
+|属性： | 功能：|
+|-|-|
+|UI Scale Mode | 确定画布中的 UI 元素的缩放方式。|
+|Constant Pixel Size | 无论屏幕大小如何，UI 元素都保持相同的像素大小。|
+|Scale With Screen Size | 屏幕越大，UI 元素越大。|
+|Constant Physical Size | 无论屏幕大小和分辨率如何，UI 元素都保持相同的物理大小。|
+
+### Constant Pixel Size 的设置
+![Alt text](image.png)
+|属性： | 功能：|
+|-|-|
+|Scale Factor | 按此系数缩放画布中的所有 UI 元素。|
+|Reference Pixels Per Unit | 如果精灵具有此“Pixels Per Unit”设置，则精灵中的每个像素将覆盖 UI 中的一个单位。|
+
+
+### Scale With Screen Size 的设置
+![Alt text](image-1.png)
+|属性： | 功能：|
+|-|-|
+|Reference Resolution | UI 布局设计的目标分辨率。如果屏幕分辨率较大，则 UI 会放大，如果较小，则 UI 会缩小。|
+|Screen Match Mode | 在当前分辨率的宽高比不适应参考分辨率时，用于缩放画布区域的模式。|
+|        Match Width or Height | 以宽度、高度或二者的某种平均值作为参考来缩放画布区域。|
+|        Expand | 水平或垂直扩展画布区域，使画布不会小于参考。|
+|        Shrink | 水平或垂直裁剪画布区域，使画布不会大于参考。|
+|Match | 确定是否以宽度、高度或二者的某种平均值作为参考进行缩放。|
+|Reference Pixels Per Unit | 如果精灵具有此“Pixels Per Unit”设置，则精灵中的每个像素将覆盖 UI 中的一个单位。|
+
+
+### Constant Physical Size 的设置：
+![Alt text](image-3.png)
+|属性： | 功能：|
+|-|-|
+|Physical Unit | 用于指定位置和大小的物理单位。|
+|Fallback Screen DPI | 在屏幕 DPI 未知时采用的 DPI。|
+|Default Sprite DPI | 用于精灵的每英寸像素，使其“Pixels Per Unit”设置与“Reference Pixels Per Unit”设置匹配。|
+|Reference Pixels Per Unit | 如果精灵具有此“Pixels Per Unit”设置，则其 DPI 将与“Default Sprite DPI”设置匹配。|
+
+## World Space Canvas 的设置（画布 (Canvas) 组件设置为 World Space 时显示）：
+|属性： | 功能：|
+|-|-|
+|Dynamic Pixels Per Unit | 用于 UI 中动态创建的位图（如文本）的每单位像素量。|
+|Reference Pixels Per Unit | 如果精灵具有此“Pixels Per Unit”设置，则精灵中的每个像素将覆盖世界中的一个单位。如果“Reference Pixels Per Unit”设置为 1，则精灵中的“Pixels Per Unit”设置将按原样使用。|
